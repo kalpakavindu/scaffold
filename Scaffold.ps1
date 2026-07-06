fb4928d1370b9dbf6b5bf084fc00b86c74e2b907
kalpakavindu <88001735+kalpakavindu@users.noreply.github.com>
2026-07-06 16:16:46 +0530

    fix: update powershell script

--- Scaffold.ps1
+++ Scaffold.ps1
@@ -1,12 +1,12 @@
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$true, HelpMessage="Enter the name of the template folder on GitHub")]
     [string]$TemplateName
 )
 
-mkdir .\scaffold-tmp
-cd .\scaffold-tmp
+New-Item -ItemType Directory -Path .\scaffold-tmp -Force | Out-Null
+Push-Location .\scaffold-tmp
 
-Write-Host "Initializing and cloning $TemplateName..."
+Write-Host "Initializing and cloning $TemplateName..." -ForegroundColor Cyan
 
 git init
 git remote add origin https://github.com/kalpakavindu/scaffold.git
@@ -14,12 +14,11 @@ git sparse-checkout init --cone
 git sparse-checkout set $TemplateName
 git pull --depth 1 origin main
 
-Write-Host "Moving files to workspace..."
+Write-Host "Moving files to workspace..." -ForegroundColor Cyan
 
-Move-Item -Path $TemplateName\* -Destination ..\
+Get-ChildItem -Path ".\$TemplateName" -Force | Move-Item -Destination ..\
 
-cd ..\
-
+Pop-Location
 Remove-Item -Path .\scaffold-tmp -Recurse -Force
 
-Write-Host "Scaffolding Complete. Happy coding!"
+Write-Host "Scaffolding Complete. Happy coding!" -ForegroundColor Green
